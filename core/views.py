6b76d122d6672adeb98b9722c0c2d552d593f5df
Oleg Vovchenko <o.vovchenko91@gmail.com>
2025-03-11 20:22:14 +0300

    templates: страница спасибо и служебная вью

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 MENU =[
     {'title': 'Главная', 'url': '/', 'active': True},
@@ -9,4 +9,10 @@ MENU =[
     {'title': 'Запись на стрижку', 'url': '/#orderForm', 'active': True},
 ]
 
+class ThanksView(TemplateView):
+    template_name = 'thanks.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = MENU
+        return context
