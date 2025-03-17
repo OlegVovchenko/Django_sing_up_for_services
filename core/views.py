ea78849780377e46749363aac74438e78b00ba30
Oleg Vovchenko <o.vovchenko91@gmail.com>
2025-03-17 21:12:05 +0300

    CreateView главной страницы

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.views.generic import TemplateView, View
 from django.shortcuts import render, redirect
 from .forms import VisitForm
 from .models import Master, Service, Visit
+from django.views.generic.edit import CreateView
 
 
 MENU =[
@@ -15,33 +16,17 @@ MENU =[
 
 class ThanksView(TemplateView):
     template_name = 'thanks.html'
+    extra_context = {'menu': MENU}
+
+class IndexView(CreateView):
+    tamplate_name = 'main.html'
+    form_class = VisitForm
+    success_url = 'thanks'
+    model = Visit
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['menu'] = MENU
+        context['masters'] = Master.objects.all()
+        context['services'] = Service.objects.all()
         return context
-
-class IndexView(View):
-    def get(self, request):
-        context = {
-            'menu': MENU,
-            'masters': Master.objects.all(),
-            'services': Service.objects.all(),
-            'form': VisitForm()
-        }
-        return render(request, 'main.html', context)
-
-    def post(self, request):
-        form = VisitForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('thanks')
-
-        context = {
-            'menu': MENU,
-            'masters': Master.objects.all(),
-            'services': Service.objects.all(),
-            'form': form
-        }
-
-        return render(request, 'main.html', {'form': form})
